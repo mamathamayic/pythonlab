287e1381db3089727ae077c5060294eb21717002
mamathamayic <mamathamayick@gmail.com>
2024-11-16 09:51:02 +0530

    hello

---
+++ circle.java
@@ -0,0 +1,12 @@
+/*compute the area circle*/
+class  area
+{
+    public static void main(String A[])
+    {
+        double pi,r,a;
+        r=10.8;
+        pi=3.14;
+        a=pi*r*r;
+        System.out.println("Area of the circle is"+a);
+    }
+}
